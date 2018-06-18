716ed85106980d969be1b893b5a048d39a354131
Eugene <yujunsung@gmail.com>
2018-06-18 16:41:06 -0400

    make login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+
+
+
+  protect_from_forgery with: :exception
+  helper_method :current_salesman, :require_login
+
+  # def cart
+  #   session[:cart] ||= []
+  # end
+
+
+
+  private
+  def current_salesman
+   @current_salesman ||= Salesman.find_by(id: session[:salesman_id]) if session[:salesman_id]
+  end
+
+  def logged_in?
+   !!current_salesman
+  end
+  def require_login
+    redirect_to root_url  unless current_salesman
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new', as: 'signin'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
-  post '/users/:id', to: 'users#show'
+  post '/salesmen/:id', to: 'salesmen#show'
   resource :sessions, only: [:new]
-  resources :users
+  resources :salesmen
   resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
