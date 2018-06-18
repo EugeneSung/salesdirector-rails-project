6e3534575bf7ca033f948665ed9ca13bf14f1410
Eugene <yujunsung@gmail.com>
2018-06-18 16:16:05 -0400

    added session controller routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+
+  def new
+    @salesman = Salesman.new
+  end
+
+def create
+
+  @salesman = User.find_by(name: params[:name])
+  if @salesman && @salesman.authenticate(params[:password])
+    session[:salesman_id] = @salesman.id
+    redirect_to salesman_path(@salesman)
+  else
+    render 'new'
+  end
+
+
+end
+
+def destroy
+
+  session.clear
+   redirect_to '/'
+
+end
 end
