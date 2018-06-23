49e945bf9320687c26dff7a5a923db36c6e47a52
Eugene <yujunsung@gmail.com>
2018-06-23 16:28:17 -0400

    added salesorder controller and views

--- app/controllers/salesorders_controller.rb
+++ app/controllers/salesorders_controller.rb
@@ -1,2 +1,62 @@
 class SalesordersController < ApplicationController
+  before_action :set_salesorder, only: [:show, :edit, :update, :destroy]
+  before_action :require_login, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @salesorder = Salesorder.all
+  end
+  def new
+    @salesorder = Salesorder.new
+  end
+  def show
+
+  end
+  def edit
+  end
+  def create
+
+
+  @salesorder = Salesorder.new(salesorder_params)
+  if(@salesorder.valid? and @salesorder.save)
+        flash[:notice] = "Order created successfully"
+        redirect_to salesorder_path(@salesorder)
+    else
+        flash[:error] = @salesorder.errors.full_messages.to_sentence
+        redirect_to action: "new"
+    end
+
+  end
+
+  def update
+  respond_to do |format|
+    if @salesorder.update(salesorder_params)
+      format.html { redirect_to @salesorder, notice: 'Order was successfully updated.' }
+    else
+      flash[:error] = @salesorder.errors.full_messages.to_sentence
+      format.html { render :edit }
+    end
+  end
+  end
+
+  def destroy
+  @salesorder.destroy
+  respond_to do |format|
+    format.html { redirect_to salesman_customer_url(current_salesman), notice: 'Order was successfully destroyed.' }
+  end
+  end
+
+
+  private
+
+  def salesorder_params
+  params.require(:salesorder).permit(:customer_id, :order_number, :salesman_id)
+  end
+  def set_salesorder
+  @salesorder = Salesorder.find(params[:id])
+  end
+
+
+
+
+
 end
--- app/models/salesorder.rb
+++ app/models/salesorder.rb
@@ -1,7 +1,7 @@
 class Salesorder < ApplicationRecord
   has_many :invoices
   belongs_to :customer
-  belongs_to :salesorder
+  belongs_to :salesman
 
   has_many :product_salesorders
   has_many :products, through: :product_salesorders
